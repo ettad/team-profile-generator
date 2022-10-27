e9a008a1ea2a22811312571c912350170b21bb2d
Tadesse Tilahun <ettad2@gmail.com>
2022-10-26 22:42:25 -0600

    added Employee test and class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require("../lib/Employee");
+
+test('creates an employee object', () => {
+
+    const employee = new Employee('Dave')
+    expect(employee.name).toBe('Dave');
+
+});
+
+test('checkes employee id', () => {
+
+    const id = 4;
+    const employee = new Employee('David', id);
+    expect(employee.id).toBe(id);
+
+});
+
+test('checks employee email', () => {
+
+    const email = 'dave@gmail.com';
+    const employee = new employee('David', 4, email);
+    expect(employee.email).toBe(email);
+
+});
+
+
+test('checks if getRole() returns employee', () => {
+
+    const role = 'Employee';
+    const employee = new Employee('David', 4, 'david@gmail.com')
+    expect(employee.getRole()).toBe(testValue);
+
+});
